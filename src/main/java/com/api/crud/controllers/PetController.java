f3fc5bb5d0c48dc507f4be192d350643e72419ab
JoaquinD26 <98410156+JoaquinD26@users.noreply.github.com>
2024-05-23 18:46:32 +0200

    Co-authored-by: zZDANTI <zZDANTI@users.noreply.github.com> Co-authored-by: jorge00ESP <jorge00ESP@users.noreply.github.com>

--- src/main/java/com/api/crud/controllers/PetController.java
+++ src/main/java/com/api/crud/controllers/PetController.java
@@ -1,7 +1,6 @@
 package com.api.crud.controllers;
 
 import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import com.api.crud.models.PetModel;
 import com.api.crud.services.PetService;
 
@@ -52,4 +53,33 @@ public class PetController {
          return "ERROR deleting the Pet with id " + id;
       }
    }
+
+   @PostMapping("/upload")
+   public String uploadFile(@RequestParam("file") MultipartFile file){
+      try{
+         return file.getOriginalFilename();
+      }catch(Exception e){
+         return "error -> " + e;
+      }
+   }
+
+
+   @PostMapping("/uploadImage/{id}")
+   public String uploadImage(@PathVariable("id") Long id, @RequestParam("image") MultipartFile image) {
+      try {
+         PetModel pet = petService.getById(id);
+         if (pet != null) {
+               pet.setPetImg(null);
+               petService.savePet(pet);
+               return "Image uploaded successfully";
+         } else {
+               return "Pet not found";
+         }
+      } catch (IOException e) {
+         return "Error uploading image: " + e.getMessage();
+      }
+   }
+
+
+
 }
