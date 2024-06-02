6135d82263d5d7b416f8a8687c48ceebc4da6545
JoaquinD26 <98410156+JoaquinD26@users.noreply.github.com>
2024-06-02 18:57:07 +0200

    god

--- src/main/java/com/api/crud/CrudApplication.java
+++ src/main/java/com/api/crud/CrudApplication.java
@@ -2,12 +2,25 @@ package com.api.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
